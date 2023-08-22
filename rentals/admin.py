76b9c876032f8c8a77f1b39037588277c3e6a53b
labeebarbisoft <muhammad.labeeb@arbisoft.com>
2023-08-22 15:22:42 +0500

    cleaned up user class

--- rentals/admin.py
+++ rentals/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from .models import Car, Driver, RideRequest
 
 
-class RideRequestModelAdmin(admin.ModelAdmin):
-    exclude = ("current_status",)
+class DriverModelAdmin(admin.ModelAdmin):
+    exclude = (
+        "last_login",
+        "is_superuser",
+        "groups",
+        "user_permissions",
+        "is_staff",
+    )
 
 
 admin.site.register(Car)
-admin.site.register(Driver)
-admin.site.register(RideRequest, RideRequestModelAdmin)
+admin.site.register(Driver, DriverModelAdmin)
+admin.site.register(RideRequest)
