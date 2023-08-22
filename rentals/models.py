43f189b46c0986cddfc6b9e79f337401d30977b8
labeebarbisoft <muhammad.labeeb@arbisoft.com>
2023-08-22 11:02:23 +0500

    created fixtures folder

--- rentals/models.py
+++ rentals/models.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
 
-class Driver(models.Model):
+class Driver(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     is_available = models.BooleanField(default=True)
