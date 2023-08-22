9e0e657015f0e9a92c5a343417773064a431f598
labeebarbisoft <muhammad.labeeb@arbisoft.com>
2023-08-22 11:03:51 +0500

    checkpoint for using the django user

--- rentals/models.py
+++ rentals/models.py
@@ -1,5 +1,12 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, User
 from django.db import models
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
 
 
 class Driver(AbstractBaseUser):
