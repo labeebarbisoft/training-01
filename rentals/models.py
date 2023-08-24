c90ba804c668a1a9ff74272970fdc820d0490019
labeebarbisoft <muhammad.labeeb@arbisoft.com>
2023-08-24 17:15:50 +0500

    checkpoint

--- rentals/models.py
+++ rentals/models.py
@@ -56,7 +56,6 @@ class Vehicle(models.Model):
     ]
     fuel_type = models.CharField(max_length=20, choices=FUEL_TYPES, blank=False)
     seating_capacity = models.IntegerField(blank=False)
-    fare_price = models.IntegerField(blank=False)
     is_active = models.BooleanField(blank=False)
 
     def __str__(self):
