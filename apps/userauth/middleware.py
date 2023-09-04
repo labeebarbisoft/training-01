a6e5565a883e73020bea80389d52192ebcbd7915
labeebarbisoft <muhammad.labeeb@arbisoft.com>
2023-09-04 16:26:57 +0500

    used middleware to prevent login of drivers

--- apps/userauth/middleware.py
+++ apps/userauth/middleware.py
@@ -1,18 +1,20 @@
 from django.shortcuts import redirect
 from django.http import Http404
+from django.contrib.auth import logout
 
 
-# class PreventDriverLoginMiddleware:
-#     def __init__(self, get_response):
-#         self.get_response = get_response
+class PreventDriverLoginMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-#     def __call__(self, request):
-#         if (
-#             request.path != "/login"
-#             and request.user.is_authenticated
-#             and request.user.profile.role == "driver"
-#             and request.user.is_superuser is False
-#         ):
-#             raise Http404
-#             return redirect("/logout")
-#         return self.get_response(request)
+    def __call__(self, request):
+        if (
+            # request.path != "/login"
+            request.user.is_authenticated
+            and request.user.profile.role == "driver"
+            and request.user.is_superuser is False
+        ):
+            logout(request)
+            return redirect("/login")
+            raise Http404("You are not allowed to login")
+        return self.get_response(request)
--- rentavehicle/settings.py
+++ rentavehicle/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # "apps.userauth.middleware.PreventDriverLoginMiddleware",
+    "apps.userauth.middleware.PreventDriverLoginMiddleware",
 ]
 
 ROOT_URLCONF = "rentavehicle.urls"
