39c127d909c7197d32d9c1dacaf3e3fa67538584
labeebarbisoft <muhammad.labeeb@arbisoft.com>
2023-10-03 12:45:36 +0500

    fixed test case post request

--- school_management/apps/api/tests.py
+++ school_management/apps/api/tests.py
@@ -1,6 +1,17 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
+from .models import (
+    School,
+    City,
+    Branch,
+    Grade,
+    Section,
+    Subject,
+    Attendance,
+    Date,
+    Student,
+)
 import base64
 import json
 
@@ -32,26 +43,55 @@ class Attendance403(TestCase):
         self.assertEqual(response.status_code, 403)
 
 
-# class Attendance200(TestCase):
-#     def setUp(self):
-#         self.username = "superuser"
-#         self.password = "superpassword"
-#         self.superuser = User.objects.create_superuser(
-#             username=self.username, password=self.password
-#         )
+class BaseTestCase(TestCase):
+    def setUp(self):
+        self.school = School.objects.create(pk=1, name="Arbisoft")
+        self.city = City.objects.create(pk=1, name="Lahore")
+        self.branch = Branch.objects.create(
+            pk=1,
+            name="Arbisoft",
+            address="25 Westwood Colony",
+            school=self.school,
+            city=self.city,
+        )
+        self.grade = Grade.objects.create(pk=1, name="10", branch=self.branch)
+        self.section = Section.objects.create(pk=1, name="A", grade=self.grade)
+        self.subject = Subject.objects.create(
+            pk=1, name="English", section=self.section
+        )
+        self.student = Student.objects.create(pk=1, name="Ali", branch=self.branch)
+        self.student.subjects.add(self.subject)
+        self.student.save()
+        self.date = Date.objects.create(date="2023-09-28")
+        self.attendance = Attendance.objects.create(
+            pk=1,
+            student=self.student,
+            subject=self.subject,
+            date=self.date,
+            status="absent",
+        )
 
-#     def test_post_request_as_superuser(self):
-#         url = reverse("attendance")
-#         post_data = {"data": {"subject": 1, "date": "2023-09-28"}}
-#         post_data = json.dumps(post_data)
-#         credentials = base64.b64encode(
-#             f"{self.username}:{self.password}".encode()
-#         ).decode()
-#         response = self.client.post(
-#             url,
-#             body=post_data,
-#             content_type="application/json",
-#             HTTP_AUTHORIZATION=f"Basic {credentials}",
-#         )
-#         print(response.content.decode("utf-8"))
-#         self.assertEqual(response.status_code, 200)
+
+class Attendance200(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.username = "superuser"
+        self.password = "superpassword"
+        self.superuser = User.objects.create_superuser(
+            username=self.username, password=self.password
+        )
+
+    def test_post_request_as_superuser(self):
+        url = reverse("attendance")
+        post_data = {"data": {"subject": 1, "date": "2023-09-28"}}
+        post_data = json.dumps(post_data)
+        credentials = base64.b64encode(
+            f"{self.username}:{self.password}".encode()
+        ).decode()
+        response = self.client.post(
+            url,
+            data=post_data,
+            content_type="application/json",
+            HTTP_AUTHORIZATION=f"Basic {credentials}",
+        )
+        self.assertEqual(response.status_code, 200)
--- school_management/apps/api/views.py
+++ school_management/apps/api/views.py
@@ -272,8 +272,7 @@ class AttendanceView(APIView):
                 status=status.HTTP_200_OK,
             )
         return Response(
-            {"Attendances": data},
-            status=status.HTTP_200_OK,
+            status=status.HTTP_400_BAD_REQUEST,
         )
 
     def put(self, request):
