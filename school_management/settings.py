20140b99873a7e75a5c52b23ba09bb98d5cc0a94
labeebarbisoft <muhammad.labeeb@arbisoft.com>
2023-10-03 15:22:16 +0500

    changed settings

--- school_management/settings.py
+++ school_management/settings.py
@@ -79,10 +79,6 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     },
-    "test": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
 }
 
 
